7ecce0e26de5102a97f40a16224d45507ff55f6a
Omistaja <Omistaja@MacBook-Air.local>
2025-06-17 13:48:34 +0300

    ready for deployment

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask
-from sqlalchemy.sql import text
 from flask import redirect, render_template, request, session
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
 
 @app.route("/")
 def index():
@@ -17,11 +15,7 @@ def index():
 
 @app.route('/etusivu')
 def etusivu():
-    sql = "SELECT text FROM text;"
-    result = db.session.execute(text(sql))
-    teksti = result.fetchall()
-    print(teksti)
-    return render_template("etusivu.html", text = teksti)
+    return render_template("etusivu.html")
 
 @app.route('/yhteystiedot')
 def yhteystiedot():
